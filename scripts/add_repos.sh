55b1b563a6e34d4066b124bacf477efea9149d43
Alexey Parfenov <alepar@gmail.com>
2013-12-09 02:03:22 -0800

    fix #145 - autoselect ubuntu mirrors in vagrant image

--- scripts/add_repos.sh
+++ scripts/add_repos.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-  apt-get -qq update
-  apt-get -y -qq install python-software-properties
-  add-apt-repository -y ppa:chris-lea/node.js
-  apt-get -qq update
+echo "deb mirror://mirrors.ubuntu.com/mirrors.txt precise main restricted universe multiverse\n\
+deb mirror://mirrors.ubuntu.com/mirrors.txt precise-updates main restricted universe multiverse\n\
+deb mirror://mirrors.ubuntu.com/mirrors.txt precise-backports main restricted universe multiverse\n\
+deb mirror://mirrors.ubuntu.com/mirrors.txt precise-security main restricted universe multiverse\n" > /etc/apt/sources.list
+
+apt-get -qq update
+apt-get -y -qq install python-software-properties
+add-apt-repository -y ppa:chris-lea/node.js
+apt-get -qq update
