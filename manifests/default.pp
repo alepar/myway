d2d21855a45c39758758be2481aa533a24947a59
Alexey Parfenov <alepar@gmail.com>
2013-12-11 00:12:53 -0800

    fixes #141: configured nginx to serve application

---
+++ manifests/default.pp
@@ -0,0 +1,22 @@
+$nginx_conf = "/etc/nginx/nginx.conf"
+$web = ["nginx", "nodejs"]
+$java = ["openjdk-7-jre"]
+
+package { $web: 
+	ensure => "installed" 
+}
+
+package { $java: 
+	ensure => "installed" 
+}
+
+file { $nginx_conf:
+	source => "/vagrant/etc/nginx.conf",
+	require => Package[$web]
+}
+
+exec { "Restart nginx":
+	subscribe => [ Package["nginx"], File[$nginx_conf] ],
+	command => "/etc/init.d/nginx restart",
+	refreshonly => true,
+}
